20c181e916f2b74ed937fb011ff35173713c26e1
Emmanuel Neri <emmanuelnerisouza@gmail.com>
2017-12-02 11:50:37 -0200

    consume message attributes

--- consumer/src/main/java/br/com/emmanuelneri/aws/sample/consumer/ConsumerListener.java
+++ consumer/src/main/java/br/com/emmanuelneri/aws/sample/consumer/ConsumerListener.java
@@ -1,15 +1,21 @@
 package br.com.emmanuelneri.aws.sample.consumer;
 
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class ConsumerListener {
 
     @JmsListener(destination = "${consumer.sqs.queue.name}")
-    public void recive(String message) {
+    public void recive(@Headers Map<String, Object> messageAttributes,
+                       @Payload String message) {
         // Do something
-        System.out.println(message);
+        System.out.println("Messages attributes: " + messageAttributes);
+        System.out.println("Body: " + message);
     }
 
 }
